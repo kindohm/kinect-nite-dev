7dd461da7f1af755caf8e6c39b75b18e8ae001f3
Mike Hodnick <mike.hodnick@gmail.com>
2011-04-04 10:18:58 -0500

    reorg start

---
+++ source/ManagedNite/FirstKinectProject/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using ManagedNite;
+
+namespace FirstKinectProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Connecting to device...");
+
+            var context = new XnMOpenNIContext();
+            var session = new XnMSessionManager(context, "Wave", "RaiseHand");
+
+            session.SessionStarted += new EventHandler<PointEventArgs>(session_SessionStarted);
+            session.SessionEnded += new EventHandler(session_SessionEnded);
+            session.FocusStartDetected += new EventHandler<FocusStartEventArgs>(session_FocusStartDetected);
+
+            Console.WriteLine("Running. Press any key to exit.");
+
+            while (!Console.KeyAvailable)
+            {
+                context.Update();
+                session.Update(context);
+            }
+        }
+
+        static void session_FocusStartDetected(object sender, FocusStartEventArgs e)
+        {
+            Console.WriteLine("Session focus progress reported.");
+        }
+
+        static void session_SessionEnded(object sender, EventArgs e)
+        {
+            Console.WriteLine("Session ended.");
+        }
+
+        static void session_SessionStarted(object sender, PointEventArgs e)
+        {
+            Console.WriteLine("Session started.");
+        }
+    }
+}
